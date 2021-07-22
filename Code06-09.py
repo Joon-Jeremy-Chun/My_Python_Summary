0dd321e1f8b3474085441de8b44709735cdc0ac3
Joon Jeremy Chun <joonchun1000@gmail.com>
2021-07-22 10:27:45 +0900

    July22(1)

---
+++ Code06-09.py
@@ -0,0 +1,46 @@
+## 함수
+def isStackFull() : # 스택 꽉 확인
+    global SIZE, stack, top
+    if (top == SIZE-1) :
+        return True
+    else :
+        return False
+
+def push(data) : # 푸쉬
+    global SIZE, stack, top
+    if (isStackFull() == True) :
+        print('스택 꽉!')
+        return
+    top += 1
+    stack[top] = data
+
+def isStackEmpty() :
+    global SIZE, stack, top
+    if (top == -1) :
+        print('empty')
+        return True
+    else:
+        print('not empty')
+        return False
+
+def pop() :
+    global SIZE, stack, top
+    if(isStackEmpty()) :
+        print('Staks already empty')
+        return None
+    data = stack[top]
+    stack[top] = None
+    top -= 1
+    return data
+
+
+## 전역
+SIZE = 5
+stack = [ None for _ in range(SIZE)]
+top = -1
+
+## 메인
+push('커피1');push('커피2')
+pop()
+pop()
+pop()
